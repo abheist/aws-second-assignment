2aa3799fc03566ab8ad6c1918bccf789245b1c7b
abheist <abhishek291093@gmail.com>
2022-06-21 22:55:22 -0300

    intermediate

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
+import axios from "axios";
 import Jimp = require("jimp");
+
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,22 +11,27 @@ import Jimp = require("jimp");
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
-        });
-    } catch (error) {
-      reject(error);
-    }
-  });
+    return new Promise(async (resolve, reject) => {
+        try {
+            let photo = await axios({
+                method: 'get',
+                url: inputURL,
+                responseType: 'arraybuffer'
+            }).then(function ({data: imageBugger}) {
+                return Jimp.read(imageBugger)
+            })
+            let outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+            await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+        } catch (error) {
+            reject(error)
+        }
+    });
 }
 
 // deleteLocalFiles
@@ -33,7 +40,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files: Array<string>) {
-  for (let file of files) {
-    fs.unlinkSync(file);
-  }
+    for (let file of files) {
+        fs.unlinkSync(file);
+    }
 }
